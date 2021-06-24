a835748149971627c57f2f734c5221158c7a7d2b
Anthony Tieu <anthonymatthewtieu@gmail.com>
2021-06-24 17:59:17 -0400

    Installed necessary modules
    -index.js moved to src folder
    -index.js can now be run

---
+++ REST-API/src/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { restart } from 'nodemon';
+const apiData = require("../../data");
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+//Endpoint for GET Franchise by ID
+app.get('/api/v1/franchises/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let franchises = apiData.data[0].franchises;
+    let response = franchises.find(franchise => franchise.id === id);
+    if (!response) {
+        res.status(404).json({ message: `Franchise with ID: ${id} doesn't exist.` });
+    }
+    res.json(response).status(200);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+})
