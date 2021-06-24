be9ca5cae2285603dc2415c8c853310fe6f4b0b6
Esh-D <deshika34@gmail.com>
2021-06-24 18:43:40 -0400

    Added 3 more endpoints
    Endpoints added:
    - GET Restaurant by ID
    - GET Reviews by Restaurant ID
    - GET Location by ID

--- REST-API/index.js
+++ REST-API/index.js
@@ -19,6 +19,43 @@ app.get('/api/v1/franchises/:id', (req, res) => {
     res.json(response).status(200);
 });
 
+//Endpoint for GET Restaurant by ID
+app.get('/api/v1/restaurants/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let restaurants = apiData.data[1].restaurants;
+    let response = restaurants.find(restaurant => restaurant.id === id);
+    if (!response) {
+        res.status(404).json({ message: `Restaurant with ID: ${id} doesn't exist.`});
+    }
+    res.json(response).status(200);
+});
+
+//Endpoint for GET Reviews by Restaurant ID
+app.get('/api/v1/restaurants/:id/reviews', (req, res) => {
+    let id = parseInt(req.params.id);
+    let restaurants = apiData.data[1].restaurants;
+    let theRestaurant = restaurants.find(restaurant => restaurant.id === id);
+
+    let reviews = theRestaurant.reviews;
+    let response = reviews;
+
+    if (!response) {
+        res.status(404).json({ message: `The ${theRestaurant.name} Review with ID: ${id} doesn't exist.`});
+    }
+    res.json(response).status(200);
+});
+
+//Endpoint for GET Location by ID
+app.get('/api/v1/locations/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let locations = apiData.data[2].locations;
+    let response = locations.find(location => location.id === id);
+    if (!response) {
+        res.status(404).json({ message: `Location with ID: ${id} doesn't exist.`});
+    }
+    res.json(response).status(200);
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
 })
