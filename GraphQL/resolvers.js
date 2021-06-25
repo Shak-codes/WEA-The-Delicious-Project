7875ea3f25930ee71bc00a214b5cfd3fe9aa025a
Anthony Tieu <anthonymatthewtieu@gmail.com>
2021-06-25 10:59:35 -0400

    Added comments to reolvers.js

--- GraphQL/resolvers.js
+++ GraphQL/resolvers.js
@@ -3,18 +3,21 @@ const fetch = require('node-fetch');
 const API_URL = 'http://localhost:3000/api/v1';
 
 const Query = {
+    // Query for GET franchise by id
     franchise: async (parent, args, context, info) => {
         const { id } = args;
         const response = await fetch(`${API_URL}/franchises/${id}`);
         const franchise = await response.json();
         return franchise;
     },
+    // Query for GET all restaurant reviews
     allReviews: async (parent, args, context, info) => {
         const { restaurant_id } = args;
         const response = await fetch(`${API_URL}/restaurants/${restaurant_id}/reviews`);
         const arrayOfReviews = await response.json();
         return arrayOfReviews;
     },
+    // Query for GET genre by id
     genre: async (parent, args, context, info) => {
         const { id } = args;
         const response = await fetch(`${API_URL}/api/v1/genres/${id}`);
@@ -28,6 +31,7 @@ const Franchise = {};
 const Restaurant = {};
 
 const Mutation = {
+    // Mutation for POST review by restaurant id, review description, and review rating
     addReview: async (parent, args, context, info) => {
         const { input } = args;
         const { restaurant_id, review_description, review_rating } = input;
