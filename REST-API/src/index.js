449ab52fa980a8a5ab75f92a7bef312abccdf800
Anthony Tieu <anthonymatthewtieu@gmail.com>
2021-06-25 22:01:48 -0400

    Fixed React Bug

--- REST-API/src/index.js
+++ REST-API/src/index.js
@@ -13,6 +13,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api/v1/franchises', franchises);
 app.use('/api/v1/restaurants', restaurants);
 app.use('/api/v1/reviews', reviews);
