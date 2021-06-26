5efeca0622ad04a9d34d266cad12f939d21f7b45
Anthony Tieu <anthonymatthewtieu@gmail.com>
2021-06-25 20:37:51 -0400

    Added review editing to REST

--- REST-API/src/routes/reviews.js
+++ REST-API/src/routes/reviews.js
@@ -35,4 +35,45 @@ router.get('/', (req, res) => {
     }
 });
 
+router.put('/', (req, res) => {
+    const { resId, revId } = req.query;
+    if (resId) {
+        if (isNaN(resId)) {
+            return res.status(400).json({ message: `Restaurant ID: ${resId} is not a number` });
+        }
+        let restaurants = apiData.data.restaurants;
+        let theRestaurant = restaurants.find(restaurant => restaurant.id === parseInt(resId));
+        if (!theRestaurant) {
+            return res.status(404).json({ message: `Restaurant with ID: ${resId} doesn't exist.` });
+        }
+        let reviews = theRestaurant.reviews;
+        if (revId) {
+            if (isNaN(revId)) {
+                return res.status(400).json({ message: `Review ID: ${revId} is not a number` });
+            }
+            let theReview = reviews.find(review => review.id === parseInt(revId));
+            if (!theReview) {
+                res.status(404).json({ message: `Review with ID: ${revId} doesn't exist for restaurant with ID: ${resId}` });
+            }
+            // Create new review object
+            const newReview = {
+                id: theReview.id,
+                description: req.body.description,
+                rating: req.body.rating
+            };
+            apiData.data.restaurants[resId - 1].reviews[theReview.id - 1] = newReview;
+            const data = JSON.stringify(apiData, null, 2);
+            fs.writeFileSync("../data.json", data, err => {
+                if (err) throw err;
+                console.log("Done Writing");
+            });
+            res.json(newReview).status(200);
+        } else {
+            res.json(reviews).status(200);
+        }
+    } else {
+        return res.status(400).json({ message: `Cannot query review without restaurant ID` });
+    }
+});
+
 module.exports = router;
